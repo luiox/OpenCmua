5f8605d5410ac4bb63aa804372c30d108cb0ca33
Canrad <1517807724@qq.com>
2025-02-07 14:06:23 +0800

    Update EventSubscribeObserver.java

--- src/main/java/me/cmua/module/EventSubscribeObserver.java
+++ src/main/java/me/cmua/module/EventSubscribeObserver.java
@@ -5,7 +5,7 @@ import me.cmua.Cmua;
 public class EventSubscribeObserver implements INotifyObserver {
     @Override
     public void notify(Module module) {
-        if (module.isOn()) {
+        if (module.isOff()) {
             Cmua.EVENT_BUS.subscribe(module);
         } else if (module.isOff()) {
             Cmua.EVENT_BUS.unsubscribe(module);
